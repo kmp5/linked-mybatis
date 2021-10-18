5bc7e5d4105d921ab9f598a0ec4d62f8a94212da
kmp5 <442764882@qq.com>
2021-10-18 11:23:57 +0800

    优化map转bean逻辑

--- jdbc-plus/src/main/java/com/kzow3n/jdbcplus/core/executor/BaseExecutor.java
+++ jdbc-plus/src/main/java/com/kzow3n/jdbcplus/core/executor/BaseExecutor.java
@@ -8,12 +8,10 @@ import lombok.Data;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.cglib.beans.BeanMap;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.CollectionUtils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -75,49 +73,60 @@ public class BaseExecutor {
     }
 
     protected <T> List<T> mapsToBeans(List<Map<String, Object>> maps, Class<T> clazz) {
-        List<Field> fields = ClazzUtils.getAllFields(clazz);
-        Map<String, String> fieldMap = fields.stream()
+        List<Field> fields = ClazzUtils.getAllFields(clazz).stream().filter(t -> !Modifier.isStatic(t.getModifiers())).collect(Collectors.toList());
+        Map<String, String> fieldClassNameMap = fields.stream()
                 .collect(Collectors.toMap(Field::getName, t -> t.getType().getName()));
-        return CollectionUtils.isEmpty(maps) ? Collections.emptyList() : maps.stream().map((e) -> mapToBean(e, clazz, fieldMap)).collect(Collectors.toList());
-    }
-
-    protected <T> T mapToBean(Map<String, Object> map, Class<T> clazz, Map<String, String> fieldMap) {
-        updateMap(map, fieldMap);
-        //BeanMap.create(bean).putAll(map);
-        return mapToObject(map, clazz);
+        Map<String, String> fieldMethodNameMap = fields.stream()
+                .collect(Collectors.toMap(Field::getName, t -> getMethodName(t.getName())));
+        return CollectionUtils.isEmpty(maps) ? Collections.emptyList() : maps.stream().map((m) -> {
+            T bean = ClassUtils.newInstance(clazz);
+            for (Field field : fields) {
+                String key = field.getName();
+                String className = fieldClassNameMap.get(key);
+                String methodName = fieldMethodNameMap.get(key);
+                if (columnCaseInsensitive) {
+                    key = key.toUpperCase();
+                }
+                if (!m.containsKey(key)) {
+                    continue;
+                }
+                Object val = m.get(key);
+                Object value = updateValue(val, className);
+                try {
+                    clazz.getMethod(methodName, field.getType()).invoke(bean, value);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return bean;
+        }).collect(Collectors.toList());
     }
 
     protected <T> T mapToBean(Map<String, Object> map, Class<T> clazz) {
-        List<Field> fields = ClazzUtils.getAllFields(clazz);
-        Map<String, String> fieldMap = fields.stream()
-                .collect(Collectors.toMap(Field::getName, t -> t.getType().getName()));
-        updateMap(map, fieldMap);
-        //BeanMap.create(bean).putAll(map);
-        return mapToObject(map, clazz);
-    }
-
-    public static <T> T mapToObject(Map<String, Object> map, Class<T> clazz){
-        T object = ClassUtils.newInstance(clazz);
-        Field[] fields = clazz.getDeclaredFields();
+        List<Field> fields = ClazzUtils.getAllFields(clazz).stream().filter(t -> !Modifier.isStatic(t.getModifiers())).collect(Collectors.toList());
+        T bean = ClassUtils.newInstance(clazz);
         for (Field field : fields) {
-            if (Modifier.isStatic(field.getModifiers())) {
-                continue;
-            }
             String key = field.getName();
-            String method = getMethodName(key);
+            String methodName = getMethodName(key);
+            String className = field.getType().getName();
+            if (columnCaseInsensitive) {
+                key = key.toUpperCase();
+            }
             if (!map.containsKey(key)) {
                 continue;
             }
+            Object val = map.get(key);
+            Object value = updateValue(val, className);
             try {
-                clazz.getMethod(method, field.getType()).invoke(object, map.get(key));
+                clazz.getMethod(methodName, field.getType()).invoke(bean, value);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return object;
+        return bean;
     }
 
-    public static String getMethodName(String s){
+    private static String getMethodName(String s) {
         StringBuilder stringBuilder = new StringBuilder("set");
         if(Character.isUpperCase(s.charAt(0)))
         {
@@ -130,91 +139,60 @@ public class BaseExecutor {
         return stringBuilder.toString();
     }
 
-    private <T> void updateMap(Map<String, Object> map, Map<String, String> fieldMap) {
-        if (columnCaseInsensitive) {
-            for (Map.Entry<String, String> entry : fieldMap.entrySet()) {
-                String key = entry.getKey();
-                String upperCaseKey = key.toUpperCase();
-                if (map.containsKey(upperCaseKey)) {
-                    map.put(key, map.remove(upperCaseKey));
-                }
+    private Object updateValue(Object val, String className) {
+        if (val == null) {
+            return null;
+        }
+        //处理String
+        if (!(val instanceof String)) {
+            if ("java.lang.String".equals(className))
+            {
+                return val.toString();
             }
         }
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-            String key = entry.getKey();
-            Object obj = entry.getValue();
-            if (obj == null) {
-                continue;
-            }
-            //处理String
-            if (!(obj instanceof String)) {
-                String className = fieldMap.get(key);
-                switch (className) {
-                    default:
-                        break;
-                    case "java.lang.String":
-                        entry.setValue(obj.toString());
-                        break;
-                }
-            }
-            //处理BigDecimal
-            if (obj instanceof BigDecimal) {
-                String className = fieldMap.get(key);
-                switch (className) {
-                    default:
-                        break;
-                    case "java.lang.Integer":
-                        entry.setValue(((BigDecimal) obj).intValue());
-                        break;
-                }
-            }
-            //处理Double
-            if (obj instanceof Double) {
-                String className = fieldMap.get(key);
-                switch (className) {
-                    default:
-                        break;
-                    case "java.math.BigDecimal":
-                        entry.setValue(BigDecimal.valueOf((double) obj));
-                        break;
-                }
-            }
-            //处理Timestamp
-            if (obj instanceof Timestamp) {
-                String className = fieldMap.get(key);
-                switch (className) {
-                    default:
-                        break;
-                    case "java.time.LocalDateTime":
-                        entry.setValue(((Timestamp) obj).toLocalDateTime());
-                        break;
-                    case "java.time.LocalDate":
-                        entry.setValue(((Timestamp) obj).toLocalDateTime().toLocalDate());
-                        break;
-                    case "java.time.LocalTime":
-                        entry.setValue(((Timestamp) obj).toLocalDateTime().toLocalTime());
-                        break;
-                }
-            }
-            //处理Integer
-            else if (obj instanceof Integer) {
-                String className = fieldMap.get(key);
-                switch (className) {
-                    default:
-                        break;
-                    case "java.lang.Boolean":
-                        if ((Integer)obj == 0) {
-                            entry.setValue(false);
-                        }
-                        else {
-                            entry.setValue(true);
-                        }
-                        break;
-                    case "java.lang.Long":
-                        entry.setValue(((Integer) obj).longValue());
-                        break;
-                }
+
+        //处理BigDecimal
+        if (val instanceof BigDecimal) {
+            switch (className) {
+                default:
+                    break;
+                case "java.lang.Integer":
+                    return ((BigDecimal) val).intValue();
             }
         }
+        //处理Double
+        else if (val instanceof Double) {
+            switch (className) {
+                default:
+                    break;
+                case "java.math.BigDecimal":
+                    return BigDecimal.valueOf((double) val);
+            }
+        }
+        //处理Timestamp
+        else if (val instanceof Timestamp) {
+            switch (className) {
+                default:
+                    break;
+                case "java.time.LocalDateTime":
+                    return ((Timestamp) val).toLocalDateTime();
+                case "java.time.LocalDate":
+                    return ((Timestamp) val).toLocalDateTime().toLocalDate();
+                case "java.time.LocalTime":
+                    return ((Timestamp) val).toLocalDateTime().toLocalTime();
+            }
+        }
+        //处理Integer
+        else if (val instanceof Integer) {
+            switch (className) {
+                default:
+                    break;
+                case "java.lang.Boolean":
+                    return (Integer) val != 0;
+                case "java.lang.Long":
+                    return ((Integer) val).longValue();
+            }
+        }
+        return val;
     }
 }
